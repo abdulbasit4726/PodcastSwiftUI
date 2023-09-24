764446e9fc8bc17d814f00ed2bce920b6c5d6a86
Abdul Basit <abdul.basit@frizhub.com>
2023-09-24 19:08:51 +0500

    Initial Commit

---
+++ Podcast-SwiftUI/Podcast_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Podcast_SwiftUIApp.swift
+//  Podcast-SwiftUI
+//
+//  Created by frizhub on 24/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Podcast_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
